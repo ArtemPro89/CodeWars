2df121026bd38829b2c14879be142f30941a2e98
ArtemPro89 <prosvirnikov1989@gmail.com>
2022-07-19 08:14:49 +0300

    Create 14.js

---
+++ 14.js
@@ -0,0 +1,8 @@
+
+function hello(name) {
+    if (typeof name === "undefined" || name.length === 0) {
+      return `Hello, World!`;
+    } else {
+      return `Hello, ${name[0].toUpperCase()}${name.substring(1,).toLowerCase()}!`;
+    }
+  }
